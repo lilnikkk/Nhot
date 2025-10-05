680cdf6e990f5598c8ca0aa29b1697be9aa024ee
NIk <127285319+lilnikkk@users.noreply.github.com>
2025-10-05 21:26:18 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  Admins: ["645753735225802752"],
+  ExpressServer: true,
+  DefaultPrefix: "%",
+  Port: 3000,
+  Website: "https://Discord-MusicBot.xbotx.repl.co",
+  SupportServer: "https://discord.gg/5uzB8bN6NV",
+  Token: process.env.Token,
+  ClientID: "878943787949371003",
+  ClientSecret: process.env.Discord_ClientSecret,
+  Scopes: ["identify", "guilds", "applications.commands"],
+  CallbackURL: "/api/callback",
+  "24/7": false,
+  CookieSecret: "Pikachu is cute",
+  IconURL:
+    "https://images-ext-2.discordapp.net/external/UvpFsqryt3iULiknKxPrJOb765orl10kqUnqIAbGZTY/https/raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif",
+  Permissions: 2205280576,
+  Lavalink: {
+    id: "Main",
+    host: "lavalink.serenetia.com",
+    port: 80,
+    pass: "https://dsc.gg/ajidevserver",
+    secure: false,
+  },
+  Spotify: {
+    ClientID: process.env.Spotify_ClientID || "bf5ee2a72bae40ffadc71a47280e5ff9",
+    ClientSecret: process.env.Spotify_ClientSecret || "053469ffeb3844079fab734ebf3090c2",
+  },
+};
